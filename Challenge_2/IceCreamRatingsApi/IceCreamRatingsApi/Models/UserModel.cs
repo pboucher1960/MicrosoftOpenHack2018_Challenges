b951209b8445d180c73b42af939982a2fa4085b4
ogvassalia <otar.gvassalia@fleetmind.com>
2018-10-31 15:32:51 -0400

    Classes adjusted

--- Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/Models/UserModel.cs
+++ Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/Models/UserModel.cs
@@ -1,4 +1,15 @@
-public class User
-{
+using System.Runtime.Serialization;
 
+namespace IceCreamRatingsApi.Models
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember(Name = "uerId")]
+        public string UserId { get; set; }
+        [DataMember(Name="userName")]
+        public string UserName { get; set; }
+        [DataMember(Name = "fullName")]
+        public string FullName { get; set; }
+    }
 }
