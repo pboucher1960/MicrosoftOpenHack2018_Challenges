903b9014a46405baae43b5ecfc6b2d3992b2cc23
Patricio <pbustos@safefleet.net>
2018-10-31 14:30:07 -0400

    Added Rating Class

---
+++ Challenge_2/IceCreamRatingsApi/Infrastructure/Rating.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure
+{
+    public class Rating
+    {
+        public IEnumerable<IRating> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRating Find(string ratingId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IRating> FindByUser(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
