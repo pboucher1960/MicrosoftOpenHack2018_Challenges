f5203307f501396e26693f245869112716a3db0b
Patricio <pbustos@safefleet.net>
2018-10-31 14:57:30 -0400

    Project File Update

--- Challenge_2/IceCreamRatingsApi/Infrastructure/Rating.cs
+++ Challenge_2/IceCreamRatingsApi/Infrastructure/Rating.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+
 
 namespace Infrastructure
 {
     public class Rating
     {
+        private const string EndpointUrl = "https://openhacktable08.documents.azure.com:10255";
+        private const string PrimaryKey = "VZFwRgdsJhhlPXgeQVCmtgTBv1W09VICD91MYUXbIyVEEQRrVYvmOKasaZW6Pax1hsKfw7XRZeEa4Hp453LxjQ==";
+        private DocumentClient client;
+
+
+        public Rating()
+        {
+
+        }
+
         public IEnumerable<IRating> Find()
         {
             throw new NotImplementedException();
