c96aac1109f4b452bd6f6c5a61582d4f7edb8cb9
Jeremy El bez <jeremy.lb@outlook.com>
2018-10-31 15:32:59 -0400

    Finish GetRatingFunction

--- Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/GetRatingFunction.cs
+++ Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/GetRatingFunction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RatingDbRepository = Infrastructure.Rating;
 
 namespace IceCreamRatingsApi
 {
@@ -30,29 +32,38 @@ namespace IceCreamRatingsApi
         /// <returns></returns>
         [FunctionName("GetRatingFunction")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
+            RatingDbRepository dbRepos = null;
             string ratingId = req.Query["ratingId"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             ratingId = ratingId ?? data?.ratingId;
 
-            return ratingId == null
-                ? new BadRequestObjectResult("Please pass a name on the query string or in the request body")
-                : (ActionResult)new OkObjectResult(new
-                {
-                    id = "79c2779e-dd2e-43e8-803d-ecbebed8972c",
-                    userId = "cc20a6fb-a91f-4192-874d-132493685376",
-                    productId = "4c25613a-a3c2-4ef3-8e02-9c335eb23204",
-                    timestamp = "2018-05-21 21:27:47Z",
-                    locationName = "Sample ice cream shop",
-                    rating = 5,
-                    userNotes = "I love the subtle notes of orange in this ice cream!"
-                });
+            if (string.IsNullOrWhiteSpace(ratingId))
+                new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            try
+            {
+                dbRepos = new RatingDbRepository();
+                var ratingFound = dbRepos.Find(ratingId);
+                if(ratingFound != null)
+                    return (ActionResult)new OkObjectResult(ratingFound);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                //dbRepos?.Close();
+            }
+
+            return new NotFoundObjectResult($"The rating cannot be found {ratingId}");
         }
     }
 }
