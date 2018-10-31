39155398157c7ae9b1b4f9a6a1136b82be3ea38a
ogvassalia <otar.gvassalia@fleetmind.com>
2018-10-31 14:22:45 -0400

    create rating created

---
+++ Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/CreateRating.cs
@@ -0,0 +1,32 @@
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace IceCreamRatingsApi
+{
+    public static class CreateRating
+    {
+        [FunctionName("CreateRating")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+
+            dynamic data = await req.Content.ReadAsAsync<object>();
+            Models.Rating rating = new Models.Rating();
+
+            rating.UserId = data?.userId;
+            rating.ProductId = data?.productId;
+
+            if (!rating.IsValid())
+            {
+                return req.CreateResponse(HttpStatusCode.BadRequest, "UserId or ProductId is missing from data");
+            }
+
+            return req.CreateResponse(HttpStatusCode.OK, "Hello");
+        }
+    }
+}
---
+++ Challenge_2/IceCreamRatingsApi/IceCreamRatingsApi/Models/Rating.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamRatingsApi.Models
+{
+    public class Rating
+    {
+        public string UserId { get; set; }
+        public string ProductId { get; set; }
+
+        public bool IsValid()
+        {
+            if (string.IsNullOrEmpty(UserId))
+            {
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(ProductId))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
