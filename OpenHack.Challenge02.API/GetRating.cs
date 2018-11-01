daecbee6b3f92029db37352d837b584ee8d70e54
pboucher <boucher.pierre@gmail.com>
2018-11-01 13:31:55 -0400

    Moved code

---
+++ OpenHack.Challenge02.API/GetRating.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using OpenHack.Challenge02.Infrastructure;
+
+namespace OpenHack.Challenge02.API
+{
+    public static class GetRating
+    {
+        /// <summary>
+        /// Example of the Respnse requsest:
+        /// 
+        /// 
+        /// {
+        ///"id": "79c2779e-dd2e-43e8-803d-ecbebed8972c",
+        ///"userId": "cc20a6fb-a91f-4192-874d-132493685376",
+        ///"productId": "4c25613a-a3c2-4ef3-8e02-9c335eb23204",
+        ///"timestamp": "2018-05-21 21:27:47Z",
+        ///"locationName": "Sample ice cream shop",
+        ///"rating": 5,
+        ///"userNotes": "I love the subtle notes of orange in this ice cream!"
+        ///}
+        /// </summary>
+        /// <param name="req"></param>
+        /// <param name="log"></param>
+        /// <returns></returns>
+        [FunctionName("GetRating")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string ratingId = req.Query["ratingId"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            ratingId = ratingId ?? data?.ratingId;
+
+            if (string.IsNullOrWhiteSpace(ratingId))
+                new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+
+            try
+            {
+                var dbRepos = new UserRatingManager();
+                //using (var dbRepos = new RatingRepository())
+                //{
+                    var ratingFound = dbRepos.Find(ratingId);
+                    if (ratingFound != null)
+                        return new OkObjectResult(ratingFound);
+                //}
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return new NotFoundObjectResult($"The rating cannot be found {ratingId}");
+        }
+    }
+}
